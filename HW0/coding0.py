9b9c7f21bda6f736141121555bae90130eefcb04
chasesadri <chasesadri1@gmail.com>
2020-10-02 12:35:26 -0700

    coding portion complete (Python)

---
+++ HW0/coding0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+AMATH 301
+Homework 0 - Coding
+
+Created on Fri Oct  2 11:52:31 2020
+
+@author: chase
+"""
+
+# Which Python dist. are we using for this class? Save answer to var problem2
+problem2 = 'Python 3.8'
