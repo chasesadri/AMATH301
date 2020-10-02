a499b72f20f3bdc261da1880b5f850e50596136a
chasesadri <chasesadri1@gmail.com>
2020-10-02 13:13:47 -0700

    Fixed writeup code style

--- HW0/writing0.py
+++ HW0/writing0.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 AMATH 301
-Homework 0 - Writeup
+Homework 0 - Writeup code
 
 Created on Fri Oct  2 11:58:05 2020
 
@@ -11,14 +11,16 @@ Created on Fri Oct  2 11:58:05 2020
 import numpy as np
 import matplotlib.pyplot as plt
 
-# x = np.arange(-5,5+0.5,0.5)
-# y = x
-# plt.plot(x, y, color='black')
-# plt.plot(x, y, color='blue', marker='o')
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.show()
+# Problem 1: Straight Line
+x = np.arange(-5,5+0.5,0.5)
+y = x
+plt.plot(x, y, color='black')
+plt.plot(x, y, color='blue', marker='o')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
 
+# Problem 2: Parabola
 x = np.arange(-5, 5 + 0.5, 0.5)
 y = x**2
 plt.plot(x, y, color = 'black')
